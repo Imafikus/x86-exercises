ff2b767695a54d46e7bbafb602818d18d88aa173
Aleksa Tešić <aleksatesicteske@gmail.com>
2018-05-13 20:54:48 +0200

    Add div_4

--- Generic examples/main.c
+++ Generic examples/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 int max(int, int);//returns greater of 2 numbers
-
+int div_4(int);//returns 1 if a % 4 == 0. 0 else
 int main()
 {
     int a, b;
     scanf("%d%d", &a, &b);
 
-    int k = max(a, b);
-    printf("max = %d\n", k);
+    int k = div_4(a);
+    
+    if(k == 1)
+        printf("is okay\n");
+    else   
+        printf("not okay\n");
 }
